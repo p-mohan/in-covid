4fbf1fd2399ec0ca657b53a38322f73bb5977179
p-mohan <prasanthmohan@yahoo.com>
2020-04-04 21:37:44 +0530

    update

--- server.js
+++ server.js
@@ -110,11 +110,11 @@ app.get('/update', function(req, res) {
        var infectionArr = [];
        let current = 0,cured = 0,death = 0;
         for(var i in innerArr){
-            if(i > innerArr.length -2) {
+            if(i > innerArr.length -3) {
                 continue;
             }
             console.log(innerArr[i]["Name of State / UT"])
-            var infection = parseInt(innerArr[i]["Total Confirmed cases (Including 57 foreign Nationals)"]);
+            var infection = parseInt(innerArr[i]["Total Confirmed cases (Including 65 foreign Nationals)"]);
             if (infection > largestInfected){
                 largestInfected = infection;
             }
