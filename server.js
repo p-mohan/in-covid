e8c4182837ae9df44cf5458a000e8bb722492373
p-mohan <prasanthmohan@yahoo.com>
2020-08-01 17:32:57 +0530

    change in format. No more confirmed from the source page.

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const mongoose = require('mongoose');
 var DayInfection = require('./models/infection.model');
 var SummaryInfection = require('./models/summary.model');
 var CronJob = require('cron').CronJob;
+const { fetch } = require("./scrapers/index.js");
 require('dotenv').config({path: __dirname + '/.env'})
 // set the port of our application
-// process.env.PORT lets the port be set by Heroku
+// process.env.PORT lets the port be set by Heroku (?=<!--)([\s\S]*?)-->
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
@@ -22,8 +23,12 @@ app.use(express.static(__dirname + '/public'));
 // Set up mongoose connection
 const mongoDB = process.env.MONGODB_URI;
 
+let regexp = /(?<=<!--)([\s\S]*?)-->/m;
 
-let stateMap = new Map();
+
+let stateMap;
+function setStates() {
+    stateMap = new Map();
 stateMap.set("Andhra Pradesh",[15.325,	78.611])
 stateMap.set("Andaman and Nicobar Islands",[12.468,	92.815])
 stateMap.set("Arunachal Pradesh",[28.179,	94.688])
@@ -52,15 +57,17 @@ stateMap.set("Punjab",[30.34,	75.094])
 stateMap.set("Rajasthan",[26.829,	73.622])
 stateMap.set("Sikkim",[27.558,	88.439])
 stateMap.set("Tamil Nadu",[10.856,	79.426])
-stateMap.set("Telangana",[17.88,	79.11])
+stateMap.set("Telengana",[17.88,	79.11])
 stateMap.set("Tripura",[23.795,	91.673])
 stateMap.set("Jammu and Kashmir",[34.03,	76.288])
 stateMap.set("Ladakh",[34.402,	78.291])
 stateMap.set("Uttar Pradesh",[27.82, 79.721])
 stateMap.set("Uttarakhand",[30.133,	78.973])
 stateMap.set("West Bengal",[23.011,	87.658])
+}
 // set the home page route
 app.get('/', function(req, res) {
+    setStates();
     var largestInfected = 0;
     var thisUpdate = new Date();
     DayInfection.findOne({}).sort({updatedAt: -1}).exec(function(err,dayInf){
@@ -69,17 +76,18 @@ app.get('/', function(req, res) {
         res.render('index');
        }
        var innerArr = dayInf.infections;
-       var infectionArr = [];
+       var confirmedArr = [];
         for(var i in innerArr){
-            console.log(innerArr[i].state)
-            var infection = innerArr[i].current;
-            if (infection > largestInfected){
-                largestInfected = infection;
+            
+            var confirmed = innerArr[i].current + innerArr[i].cured + innerArr[i].death;
+            if (confirmed > largestInfected){
+                largestInfected = confirmed;
             }
             if(stateMap.get(innerArr[i].state) === undefined) {
                 stateMap.set(innerArr[i].state,[18.98,	67.76])
             }
-            stateMap.get(innerArr[i].state).push(infection);
+            console.log(innerArr[i].state, confirmed);
+            stateMap.get(innerArr[i].state).push(confirmed);
         }
         console.log(largestInfected);
         fillSummaryInf(res,largestInfected);
@@ -94,7 +102,7 @@ function fillSummaryInf(res,largestInfected){
     var days = [];
     var current = [];
     var mortality = [];
-    SummaryInfection.find({}).sort({day: 1}).exec(function(err,dayInf){
+    SummaryInfection.find({ }).sort({day: 1}).exec(function(err,dayInf){ //day: {$gte: new Date(2020, 5, 1)}
      //   console.log("dayInf",dayInf);
         if(err) {
          console.error(err);
@@ -102,13 +110,22 @@ function fillSummaryInf(res,largestInfected){
         for(var i in dayInf) {
             let date1 = dayInf[i].day;
             let month = parseInt(date1.substring(date1.indexOf("-")+1,date1.lastIndexOf("-")))
+            if( month < 5) {
+                continue;
+            }
+            let dayofMonth = parseInt(date1.substring(date1.lastIndexOf("-")+1))
             if(month < 7) {
                 let day = parseInt(date1.substring(date1.lastIndexOf("-")+1));
                 if(!isOdd(day))
                     continue;
             }
             days.push(date1.substring(date1.indexOf('-')+1));
-            current.push(dayInf[i].infections[0].current);
+            if ((month == 7 && dayofMonth > 26) || month > 7) {
+                console.log(dayInf[i].infections[0].current , dayInf[i].infections[0].cured , dayInf[i].infections[0].death)
+                current.push(dayInf[i].infections[0].current + dayInf[i].infections[0].cured +  dayInf[i].infections[0].death);
+            } else {
+                current.push(dayInf[i].infections[0].current);
+            }
             mortality.push(dayInf[i].infections[0].death);
         }
         console.log("days",days);
@@ -122,84 +139,99 @@ function isOdd(num) { return num % 2;}
  function  update(res) {
     var largestInfected = 0;
     var thisUpdate = new Date();
-    rp('https://www.mohfw.gov.in/')
-    .then(function (htmlString) {
-        $ = cheerio.load(htmlString)
-        obj = $('#state-data > div > div > div > div');
-       // console.log(obj.html())
-       const converted = tabletojson.convert(obj.html());
-       var innerArr = converted[0];
-       var infectionArr = [];
-       let current = 0,cured = 0,death = 0;
-       for (var i = 0; i < innerArr.length; i++){
 
-           let sn = innerArr[i][Object.keys(innerArr[i])[0]]; //S. No
-           if (!/^\d+?/.exec(sn)) {
-               continue;
-           }
-            console.log(">>>>>>>"+sn+"<<<<<<<<<<<<<<<<");
-            var infection = parseInt(innerArr[i][Object.keys(innerArr[i])[5]]);
-            if(infection == 0) {
-                continue;
-            }
-            if (infection > largestInfected){
-                largestInfected = infection;
-            }
-            let state = innerArr[i][Object.keys(innerArr[i])[1]];
-            state = state.replace(/[^a-zA-Z ]/g, "")
-            if(stateMap.get(state) === undefined) {
-                stateMap.set(state,[18.98,	67.76])
-            }
-            stateMap.get(state).push(infection);
-            var item = {
-            state: state,
-            current: infection,
-            cured: parseInt(innerArr[i][Object.keys(innerArr[i])[3]].replace(/[^0-9 ]/g, "")),
-            death: parseInt(innerArr[i][Object.keys(innerArr[i])[4]].replace(/[^0-9 ]/g, ""))  
+    fetch(
+        'https://www.mohfw.gov.in/',
+        error => {
+          console.log(error);
+        },
+        htmlString => {
+            try {
+                
+                $ = cheerio.load(htmlString)
+                obj = $('#state-data > div > div > div > div');
+                let body = obj.html()//.match(regexp);
+               console.log(body)
+               const converted = tabletojson.convert("<table>"+body+"</table>");
+               var innerArr = converted[0];
+               var confirmedArr = [];
+               let current = 0,cured = 0,death = 0;
+               for (var i = 0; i < innerArr.length; i++){
+                   const rowVal = Object.values(innerArr[i]);
+                   let sn = rowVal[2]; //S. No
+                   if (!/^\d+?/.exec(sn)) {
+                       continue;
+                   }
+                    console.log(">>>>>>>"+sn+"<<<<<<<<<<<<<<<<");
+                    var confirmed = parseInt(rowVal[4]);
+                    if(confirmed == 0) {
+                        continue;
+                    }
+                    if (confirmed > largestInfected){
+                        largestInfected = confirmed;
+                    }
+                    let state = rowVal[3];
+                    state = state.replace(/[^a-zA-Z ]/g, "")
+                    if(state == '') {
+                        continue;
+                    }
+                    if(stateMap.get(state) === undefined) {
+                        stateMap.set(state,[18.98,	67.76])
+                    }
+                    stateMap.get(state).push(confirmed);
+                    var item = {
+                    state: state,
+                    current: confirmed,
+                    cured: parseInt(rowVal[6].replace(/[^0-9 ]/g, "")),
+                    death: parseInt(rowVal[0].replace(/[^0-9 ]/g, ""))  
+                }
+                    console.log(item);
+                    current += item.current;
+                    cured += item.cured;
+                    death += item.death;
+                    confirmedArr.push( item);
+                }
+                var inf = new DayInfection({
+                    infections: confirmedArr,
+                    updatedAt: thisUpdate
+                });
+            
+                inf.save(function (err) {
+                    if (err) {
+                        console.log(err)
+                    }
+                });
+               
+                 SummaryInfection.update({day: thisUpdate.toISOString().slice(0,10)}, {
+                    infections:  [{
+                        state: "Total",
+                        current: current,
+                        cured: cured,
+                        death: death        
+                    }],
+                    day: thisUpdate.toISOString().slice(0,10)
+                }, { upsert : true}, (err, doc) => {
+                    if (err) {
+                        console.log("Something wrong when updating data!");
+                    }
+                
+                    console.log(doc);
+                })
+                fillSummaryInf(res,largestInfected);
+                
+            } catch (err) {
+                // Crawling failed...
+                throw err;
+            };
         }
-            console.log(item);
-            current += item.current;
-            cured += item.cured;
-            death += item.death;
-            infectionArr.push( item);
-        }
-        var inf = new DayInfection({
-            infections: infectionArr,
-            updatedAt: thisUpdate
-        });
-    
-        inf.save(function (err) {
-            if (err) {
-                console.log(err)
-            }
-        });
-       
-         SummaryInfection.update({day: thisUpdate.toISOString().slice(0,10)}, {
-            infections:  [{
-                state: "Total",
-                current: current,
-                cured: cured,
-                death: death        
-            }],
-            day: thisUpdate.toISOString().slice(0,10)
-        }, { upsert : true}, (err, doc) => {
-            if (err) {
-                console.log("Something wrong when updating data!");
-            }
-        
-            console.log(doc);
-        })
-        fillSummaryInf(res,largestInfected);
-        
-    })
-    .catch(function (err) {
-        // Crawling failed...
-        throw err;
-    });
+      );
+
+
 }
 
 app.get('/update', function(req, res) {
         try {
+            setStates();
             // ejs render automatically looks in the views folder
             update(res);
         }
